d69653bd391d84d8196907d10672c053db00539d
hjx <1439140519@qq.com>
2024-04-20 00:20:50 +0800

    update default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-import {useEffect} from "react";
-import {useRouter} from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/share'); // 子页面的路径
-  }, []);
-
   return null;
 }
